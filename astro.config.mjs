b55b7451f4536763d9da2ef1ef0b346151cc764c
juancarlos <fornielesjc@gmail.com>
2024-07-04 18:46:31 +0200

    prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fornielesjc.github.io',
-  base:'portfolio-dev',
   integrations: [tailwind()]
 });
